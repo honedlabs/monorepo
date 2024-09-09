624ad0336fb084babf77b2057e2e0ae06888966c
jdw5 <joshuadwallace1@gmail.com>
2024-09-09 21:18:48 +1000

    Removed bool based attributes from attribute checking to reduce reflection calls

--- workbench/app/Attributable.php
+++ workbench/app/Attributable.php
@@ -2,7 +2,33 @@
 
 namespace Workbench\App;
 
+use Conquest\Table\Actions\Attributes\Key;
+use Conquest\Table\Actions\Attributes\Name;
+use Conquest\Table\Actions\Attributes\Type;
+use Conquest\Table\Actions\Attributes\Label;
+use Conquest\Table\Actions\Attributes\Title;
+use Conquest\Table\Actions\Attributes\Value;
+use Conquest\Table\Actions\Attributes\Format;
+use Conquest\Table\Actions\Attributes\Property;
+use Conquest\Table\Actions\Attributes\Validate;
+use Conquest\Table\Actions\Attributes\Authorize;
+use Conquest\Table\Actions\Attributes\Transform;
+use Conquest\Table\Actions\Attributes\Description;
+use Conquest\Table\Actions\Attributes\Placeholder;
 
+#[Authorize(fn () => true)]
+#[Description('Description')]
+#[Format('d M y')]
+#[Key('id')]
+#[Label('Label')]
+#[Name(('Name'))]
+#[Placeholder('Placeholder')]
+#[Property('property')]
+#[Title('Title')]
+#[Transform(fn () => 'transformed')]
+#[Type('type')]
+#[Validate(fn () => true)]
+#[Value('value')]
 class Attributable extends Component
 {
 
