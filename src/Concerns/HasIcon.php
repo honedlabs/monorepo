0101c731b69d8abcf73fd39185e35e857e1caa9c
jdw5 <joshuadwallace1@gmail.com>
2024-12-20 00:02:16 +1000

    Icon trait and test

---
+++ src/Concerns/HasIcon.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Honed\Core\Concerns;
+
+trait HasIcon
+{
+    /**
+     * @var string|\Closure():string|null
+     */
+    protected $icon;
+
+    /**
+     * Set the icon, chainable.
+     *
+     * @param string|\Closure():string|null $icon
+     * @return $this
+     */
+    public function icon(string|\Closure $icon): static
+    {
+        $this->setIcon($icon);
+
+        return $this;
+    }
+
+    /**
+     * Set the icon, quietly.
+     *
+     * @param string|\Closure():string|null $icon
+     */
+    public function setIcon(string|\Closure|null $icon): void
+    {
+        $this->icon = $icon;
+    }
+
+    /**
+     * Determine if the instance has an icon.
+     */
+    public function hasIcon(): bool
+    {
+        return ! \is_null($this->icon);
+    }
+
+    /**
+     * Get the icon.
+     */
+    public function getIcon(): ?string
+    {
+        return value($this->icon);
+    }
+}
