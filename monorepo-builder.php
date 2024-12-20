b065a24f65e31ea35cb6a6cb0b4bdc4b859e606e
jdw5 <joshuadwallace1@gmail.com>
2024-12-20 17:45:50 +1000

    .

--- monorepo-builder.php
+++ monorepo-builder.php
@@ -3,8 +3,17 @@
 declare(strict_types=1);
 
 use Symplify\MonorepoBuilder\Config\MBConfig;
+use Symplify\MonorepoBuilder\Release\ReleaseWorker\SetCurrentMutualDependenciesReleaseWorker;
+use Symplify\MonorepoBuilder\Release\ReleaseWorker\SetNextMutualDependenciesReleaseWorker;
+use Symplify\MonorepoBuilder\Release\ReleaseWorker\UpdateBranchAliasReleaseWorker;
 
-return static function (MBConfig $mbConfig): void {
-    $mbConfig->packageDirectories([__DIR__ . '/packages/laravel']);
-    $mbConfig->packageAliasFormat('<major>.<minor>.x-dev');
+return static function (MBConfig $config): void {
+    $config->packageDirectories([__DIR__ . '/packages/laravel']);
+    $config->packageAliasFormat('<major>.<minor>.x-dev');
+
+    $config->workers([
+        SetCurrentMutualDependenciesReleaseWorker::class,
+        SetNextMutualDependenciesReleaseWorker::class,
+        UpdateBranchAliasReleaseWorker::class,
+    ]);
 };
