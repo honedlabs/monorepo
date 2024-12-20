58fc4e63979ac06c4a940ddb5b450d06e6f60c63
jdw5 <joshuadwallace1@gmail.com>
2024-12-20 21:09:00 +1000

    Laravel stub

---
+++ stubs/laravel/src/Facades/PackageName.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace VendorName\PackageName\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+/**
+ * @see \VendorName\PackageName\PackageName
+ */
+class PackageName extends Facade
+{
+    protected static function getFacadeAccessor(): string
+    {
+        return \VendorName\PackageName\PackageName::class;
+    }
+}
