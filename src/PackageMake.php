f89d24de84e2db972bda2461a1f301b88d80bf60
jdw5 <joshuadwallace1@gmail.com>
2024-12-20 20:46:51 +1000

    Directory copy command

--- src/PackageMake.php
+++ src/PackageMake.php
@@ -2,7 +2,7 @@
 
 namespace Honed\Honed;
 
-use function Laravel\Prompts\{suggest, text, multiselect};
+use function Laravel\Prompts\{error, info, text, multiselect, outro};
 
 
 class PackageMake
@@ -41,8 +41,8 @@ class PackageMake
     {
         $this->types = multiselect(
             label: 'What type of package(s) are you making?',
-            options: \array_keys($this->packageTypes()),
-            default: ['Laravel'],
+            options: $this->packageTypes(),
+            default: ['laravel'],
             hint: 'You can select multiple types.',
             required: true
         );
@@ -56,15 +56,125 @@ class PackageMake
 
         $this->slug = \strtolower(\str_replace(' ', '-', $this->name));
 
-        dd($this->types, $this->name, $this->slug);
+        $this->description = text(
+            label: 'Enter a description for the package.',
+            placeholder: 'Create server-driven...',
+            required: false,
+        );
+
+        foreach($this->types as $type) {
+            $this->buildPackage($type);
+        }
+
+        outro(\sprintf('Successfully created [%d] package(s) for [%s].', \count($this->types), $this->slug));
+        exit(0);
     }
 
-    public function packageTypes(): array
+    protected function buildPackage(string $type): void
+    {
+        $path = $this->stubPath($type);
+        $directory = $this->packageDirectory($type);
+
+        // Check if there is a directory there
+        if(\is_dir($directory)) {
+            error(\sprintf("There already exists a [%s] package for [%s].", $type, $this->slug));
+            exit(1);
+        }
+
+        $result = $this->copyDir($path, $directory);
+
+        if (! $result) {
+            error(\sprintf("Failed to copy [%s] to [%s].", $path, $directory));
+            exit(1);
+        }
+
+        if (! $this->replaceInDirectory($directory)) {
+            error(\sprintf("Failed to execute replacements in [%s] for [%s].", $directory, $this->slug));
+            exit(1);
+        }
+
+        info(\sprintf("Successfully created [%s] package for [%s].", $type, $this->slug));
+    }
+
+    protected function copyDir(string $source, string $destination, int $permissions = 0755): bool
+    {
+        $hash = $this->hashDirectory($source);
+
+        if (\is_link($source)) {
+            return \symlink(\readlink($source), $destination);
+        }
+
+        if (\is_file($source)) {
+            return \copy($source, $destination);
+        }
+
+        if (! \is_dir($destination)) {
+            \mkdir($destination, $permissions, true);
+        }
+
+        $dir = \dir($source);
+
+        while (false !== ($file = $dir->read())) {
+            if ($file == '.' || $file == '..') {
+                continue;
+            }
+
+            if ($hash != $this->hashDirectory($source . '/' . $file)) {
+                $this->copyDir($source . '/' . $file, $destination . '/' . $file, $permissions);
+            }
+        }
+
+        $dir->close();
+        return true;
+    }
+
+    protected function replaceInDirectory(string $directory): bool
+    {
+        // Search the entire directory and perform replacements
+        return true;
+    }
+
+    protected function hashDirectory(string $directory): string
+    {
+        if (! \is_dir($directory)) { 
+            return false;
+        }
+
+        $files = [];
+        $dir = \dir($directory);
+
+        while (false !== ($file = $dir->read())){
+            if ($file != '.' and $file != '..') {
+                if (\is_dir($directory . '/' . $file)) { $files[] = $this->hashDirectory($directory . '/' . $file); }
+                else { $files[] = md5_file($directory . '/' . $file); }
+            }
+        }
+
+        $dir->close();
+
+        return md5(implode('', $files));
+    }
+
+    protected function packageTypes(): array
     {
         return [
-            'Laravel' => 'path',
-            'Vue' => 'path',
-            'Typescript' => 'path',
+            'laravel' => 'Laravel',
+            'vue' => 'Vue',
+            'typescript' => 'Typescript',
         ];
+    }
+
+    protected function packageDirectory(string $type): string
+    {
+        return __DIR__ . '/../packages/' . $type . '/' . $this->slug;
+    }
+
+    protected function stubPath(string $type): string
+    {
+        return match($type) {
+            'laravel' => __DIR__ . '/../stubs/laravel',
+            'vue' => __DIR__ . '/../stubs/vue',
+            'typescript' => __DIR__ . '/../stubs/typescript',
+        };
     }
 }
