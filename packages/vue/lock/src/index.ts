42c0ec9fd7b519f86a4b95f5e200751c6f6a5bf1
jdw5 <joshuadwallace1@gmail.com>
2024-12-21 12:06:19 +1000

    feat: can, canAny helper functions

---
+++ packages/vue/lock/src/index.ts
@@ -0,0 +1,12 @@
+import { usePage } from "@inertiajs/vue3";
+import { type Page } from "@inertiajs/core";
+
+const page = usePage() as Page<{ auth: { abilities: Record<string, any> } }>;
+
+export const can = (ability: string, resource: any = null): boolean => 
+    (resource ? page.props.auth.abilities[ability][resource]
+    : page.props.auth.abilities[ability]) ?? false;
+
+export const canAny = (abilities: string|string[], resource: any = null): boolean => 
+    Array.isArray(abilities) ? abilities.some(ability => can(ability, resource))
+        : can(abilities, resource);
