bc53eb4e9a4a8a9bc59e950eaee9daef507d664a
jdw5 <joshuadwallace1@gmail.com>
2025-01-05 22:00:27 +1000

    feat(table): prepping has pages table to enable page options

--- packages/laravel/table/src/Concerns/HasPages.php
+++ packages/laravel/table/src/Concerns/HasPages.php
@@ -5,11 +5,14 @@ declare(strict_types=1);
 namespace Honed\Table\Concerns;
 
 use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Contracts\Pagination\Paginator;
 use Illuminate\Contracts\Pagination\CursorPaginator;
 use Honed\Table\Exceptions\InvalidPaginatorException;
 use Illuminate\Contracts\Pagination\LengthAwarePaginator;
+use Illuminate\Http\Request;
+use Illuminate\Pagination\CursorPaginator as PaginationCursorPaginator;
+use Illuminate\Pagination\LengthAwarePaginator as PaginationLengthAwarePaginator;
+use Illuminate\Support\Collection;
 
 trait HasPages
 {
@@ -29,12 +32,12 @@ trait HasPages
     protected static $defaultPerPageAmount = 10;
 
     /**
-     * @var class-string|null
+     * @var 'cursor'|'simple'|'length-aware'|class-string<\Illuminate\Contracts\Pagination\Paginator>|null
      */
     protected $paginator;
 
     /**
-     * @var class-string|null
+     * @var 'cursor'|'simple'|'length-aware'|class-string<\Illuminate\Contracts\Pagination\Paginator>
      */
     protected static $defaultPaginator = LengthAwarePaginator::class;
 
@@ -44,6 +47,8 @@ trait HasPages
     protected $page;
 
     /**
+     * Use the default page key.
+     * 
      * @var string|null
      */
     protected static $pageKey = null;
@@ -59,7 +64,7 @@ trait HasPages
     protected static $countKey = 'show';
 
     /**
-     * Configure the options for the number of items to show per page.
+     * Configure the options for the number of records to show per page.
      *
      * @param  int|array<int,int>  $perPage
      */
@@ -69,138 +74,114 @@ trait HasPages
     }
 
     /**
-     * Configure the default paginator to use.
+     * Configure the paginator to use.
      *
-     * @param  string|\Honed\Table\Enums\Paginator  $paginator
+     * @param 'cursor'|'simple'|'length-aware'|class-string<\Illuminate\Contracts\Pagination\Paginator> $paginator
      */
-    public static function usePaginator(string|Paginator $paginator): void
+    public static function usePaginator(string $paginator): void
     {
         static::$defaultPaginator = $paginator;
     }
 
     /**
-     * Configure the query parameter to use for the page number.
+     * Configure the query parameter name to use for the current page number being shown.
      */
-    public static function usePageKey(string $name): void
+    public static function usePageKey(?string $name): void
     {
         static::$pageKey = $name;
     }
 
     /**
-     * Get the options for the number of items to show per page.
+     * Get the options for the number of records to show per page.
      *
      * @return int|non-empty-array<int,int>
      */
     public function getPerPage(): int|array
     {
         return match (true) {
-            \property_exists($this, 'perPage') => $this->perPage,
+            \property_exists($this, 'perPage') && ! \is_null($this->perPage) => $this->perPage,
             \method_exists($this, 'perPage') => $this->perPage(),
             default => static::$defaultPerPageAmount
         };
     }
 
     /**
-     * Get the default paginator to use.
-     *
-     * @return class-string|null
+     * Get the paginator to use.
+     * 
+     * @return 'cursor'|'simple'|'length-aware'|class-string<\Illuminate\Contracts\Pagination\Paginator>|null
      */
-    public function getPaginator(): string|null
+    public function getPaginator(): ?string
     {
         return match (true) {
-            \property_exists($this, 'paginator') => $this->paginator,
+            \property_exists($this, 'paginator') && ! \is_null($this->paginator) => $this->paginator,
             \method_exists($this, 'paginator') => $this->paginator(),
             default => static::$defaultPaginator
         };
     }
 
     /**
-     * Get the query parameter to use for the page number.
+     * Get the query parameter to use for the current page number being shown.
      */
-    public function getPageKey(): string
+    public function getPageKey(): ?string
     {
         return match (true) {
-            \property_exists($this, 'page') => $this->page,
+            \property_exists($this, 'page') && ! \is_null($this->page) => $this->page,
+            \method_exists($this, 'page') => $this->page(),
             default => static::$pageKey
         };
     }
 
     /**
-     * Get the query parameter to use for the number of items to show.
+     * Get the query parameter to use for the number of records to show per page.
      */
     public function getCountKey(): string
     {
         return match (true) {
-            \property_exists($this, 'count') => $this->count,
+            \property_exists($this, 'count') && ! \is_null($this->count) => $this->count,
             \method_exists($this, 'count') => $this->count(),
             default => static::$countKey
         };
     }
 
     /**
-     * Get the pagination options for the number of items to show per page.
-     *
-     * @return array<int,array{value:int,active:bool}>
-     */
-    public function getPaginationCounts(int|null $active = null): array
-    {
-        $perPage = $this->getRecordsPerPage();
-
-        return is_array($perPage)
-            ? array_map(fn ($count) => ['value' => $count, 'active' => $count === $active], $perPage)
-            : [['value' => $perPage, 'active' => true]];
-    }
-
-    public function getRecordsPerPage(): int|false
-    {
-        $request = request();
-
-        if (\is_null($this->getPaginator())) {
-            return false;
-        }
-
-        // Only an array can have pagination options, so short circuit if not an array
-        if (! \is_array($this->getPerPage())) {
-            return $this->getPerPage();
-        }
-
-        // Force integer
-        $fromRequest = $request->integer($this->getPerPageName());
-
-        // Loop over the options to create a serializable array
-
-        // Must ensure the query param is in the array to prevent abuse of 1000s of records
-
-        // 0 indicates no term is provided, so use the first option
-        if ($fromRequest === 0) {
-            return $this->getPerPage()[0];
-        }
-
-        return $this->getPerPage();
-    }
-
-    /**
      * Execute the query and paginate the results.
+     * 
+     * @throws \Honed\Table\Exceptions\InvalidPaginatorException
+     * @return \Illuminate\Contracts\Pagination\LengthAwarePaginator|\Illuminate\Contracts\Pagination\Paginator|\Illuminate\Contracts\Pagination\CursorPaginator|\Illuminate\Support\Collection
      */
-    public function paginateRecords(Builder $query): Paginator|CursorPaginator|Collection
-    {
-        $paginator = match ($this->getPaginator()) {
-            LengthAwarePaginator::class => $query->paginate(
-                perPage: $this->getRecordsPerPage(),
+    public function paginateRecords(Builder $query, Request $request = null): mixed
+    {        
+        $paginator = $this->getPaginator();
+
+        $paginated = match (true) {
+            \in_array($paginator, ['length-aware',
+                LengthAwarePaginator::class,
+                PaginationLengthAwarePaginator::class,
+            ]) => $query->paginate(
+                perPage: $this->getRecordsPerPage($request),
                 pageName: $this->getPageKey(),
             ),
-            Paginator::class => $query->simplePaginate(
-                perPage: $this->getRecordsPerPage(),
+            \in_array($paginator, ['simple', Paginator::class]) => $query->simplePaginate(
+                perPage: $this->getRecordsPerPage($request),
                 pageName: $this->getPageKey(),
             ),
-            CursorPaginator::class => $query->cursorPaginate(
-                perPage: $this->getRecordsPerPage(),
+            \in_array($paginator, ['cursor', CursorPaginator::class, PaginationCursorPaginator::class]) => $query->cursorPaginate(
+                perPage: $this->getRecordsPerPage($request),
                 cursorName: $this->getPageKey(),
             ),
-            null => $query->get(),
-            default => throw new InvalidPaginatorException($this->getPaginator()),
+            \in_array($paginator, [null, 
+                'none', 
+                'collection', 
+                Collection::class
+            ]) => $query->get(),
+            default => throw new InvalidPaginatorException($paginator),
         };
 
-        return $paginator->withQueryString();
+        return $paginated->withQueryString();
+    }
+
+    public function getRecordsPerPage(Request $request = null)
+    {
+
     }
 }
