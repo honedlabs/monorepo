62ffa7a0b36b0d747102d444207883710687da7c
jdw5 <joshuadwallace1@gmail.com>
2025-01-30 21:44:57 +1000

    feat(refine): composable handles search matches

--- packages/vue/refine/src/refinements.ts
+++ packages/vue/refine/src/refinements.ts
@@ -6,8 +6,6 @@ import { type CalendarDateTime } from '@internationalized/date'
 
 export type SortDirection = 'asc' | 'desc'
 
-export type AvailableVisitOptions = Omit<VisitOptions, 'url' | 'data'>
-
 export interface Refinement {
     name: string
     label: string
@@ -21,20 +19,11 @@ export interface FilterRefinement extends Refinement {
     value: any
 }
 
-export type FilterBinding<T extends FilterRefinement, U extends any = any> = 
-	T extends { type: 'similar' } ? string :
-		T extends { type: 'boolean' } ? boolean :
-			T extends { type: 'set', multiple: true } ? U[] :
-				T extends { type: 'date' } 
-					? CalendarDateTime|null
-					: U
-
 export interface SortRefinement extends Refinement {
     type: 'sort'
     direction?: SortDirection
     next?: SortDirection
 }
-
 
 export interface SearchRefinement extends Refinement {
     type: 'search'
@@ -44,73 +33,73 @@ export interface Refinements {
     filters: FilterRefinement[]
     sorts: SortRefinement[]
     searches: SearchRefinement[]
-	search: string
-	columns?: string[]
+	search: {
+		value: string
+		matches: string[]
+	}
     keys: {
         sorts: string
         search: string
-        columns: string
+        matches: string
     }
-}
-
-export interface RefinementOptions extends VisitOptions {
-	debounce?: number
-	watch?: boolean
 }
 
 export interface SortOptions extends VisitOptions {
 	direction?: SortDirection
 }
 
+export type FilterBinding<T extends FilterRefinement, U extends any = any> = 
+	T extends { type: 'similar' } ? string :
+		T extends { type: 'boolean' } ? boolean :
+			T extends { type: 'set', multiple: true } ? U[] :
+				T extends { type: 'date' } 
+					? CalendarDateTime|null
+					: U
+
 export function useRefinements<
 	T extends object,
 	K extends { [U in keyof T]: T[U] extends Refinements ? U : never }[keyof T],
 >(
-	props: T, 
+	props: T,
     key: K,
-    options: RefinementOptions = {}
+    defaultOptions: VisitOptions = {}
 ) {
 	const refinements = computed(() => props[key] as Refinements)
 
 	const sortsKey = computed(() => refinements.value.keys.sorts)
     const searchKey = computed(() => refinements.value.keys.search)
-    const columnsKey = computed(() => refinements.value.keys.columns)
+    const matchesKey = computed(() => refinements.value.keys.matches)
 
     const currentSort = computed(() => refinements.value.sorts.find(({ active }) => active))
 	const currentFilters = computed(() => refinements.value.filters.filter(({ active }) => active))
+	const currentSearches = computed(() => refinements.value.searches.filter(({ active }) => active))
+	
+	const state = reactive<Record<
+		typeof searchKey.value, string
+	> & Record<
+		typeof sortsKey.value, SortDirection | undefined
+	> & Record<
+		typeof matchesKey.value, string[] | undefined
+	> & {
+		[F in Refinements['filters'][number] as F['name']]: FilterBinding<F>
+	}>({})
 
-	const state = reactive<Record<string, any>>({})
-
-	function initializeState() {
-		state[searchKey.value] = refinements.value.search
+	function updateState() {
+		state[searchKey.value] = refinements.value.search.value
 		state[sortsKey.value] = currentSort.value?.direction
-		state[columnsKey.value] = refinements.value.columns
+		state[matchesKey.value] = refinements.value.search.matches
 		
 		refinements.value.filters.forEach((filter) => {
 			state[filter.name] = filter.value
 		})
 	}
 
-	initializeState()
-
-	// watchPausable(refinements.value, () => {
-	// 	initializeState()
-	// }, {
-	// 	immediate: true,
-	// })
-
-	const {
-		watch = true,
-		...visitOptions
-	} = options
-
-
-
     function refine(options: VisitOptions = {}) {
         const data: Record<string, any> = {}
         
         for (const key in state) {
             const value = state[key]
+
             if ([null, '', []].includes(value)) {
                 data[key] = undefined
             } else if (Array.isArray(value)) {
@@ -120,26 +109,26 @@ export function useRefinements<
             }
         }
 
+		data[searchKey.value] = (data[searchKey.value] ?? '').toLowerCase().replace(/[\s./]/g, '+')
+
         router.reload({
-            ...visitOptions,
+            ...defaultOptions,
             ...options,
             data
         })
     }
 
-	function getSort(name: string): SortRefinement | undefined {
-		return refinements.value.sorts.find((sort) => sort.name === name)
-	}
-
 	function getFilter(name: string): FilterRefinement | undefined {
 		return refinements.value.filters.find((sort) => sort.name === name)
 	}
 
-	function applySearch(value: string, options: VisitOptions = {}) {
-		state[searchKey.value] = value
-		return refine(options)
+	function getSort(name: string): SortRefinement | undefined {
+		return refinements.value.sorts.find((sort) => sort.name === name)
 	}
 
+	function getMatch(name: string): SearchRefinement | undefined {
+		return refinements.value.searches.find((search) => search.name === name)
+	}
 
 	function applyFilter(name: string, value: any, options: VisitOptions = {}) {
 		const filter = getFilter(name)
@@ -167,12 +156,26 @@ export function useRefinements<
 		return refine(options)
 	}
 
-	function isSorting(name?: string, direction?: SortDirection): boolean {
-		if (name) {
-			return refinements.value.sorts.some((sort) => sort.name === name && (direction ? sort.direction === direction : true))
+	function applySearch(value: string, options: VisitOptions = {}) {
+		state[searchKey.value] = value
+
+		return refine(options)
+	}
+
+	function applyMatch(name: string, options: VisitOptions = {}) {
+		if (! Array.isArray(state[matchesKey.value])) {
+			state[matchesKey.value] = []
 		}
 
-		return !!currentSort.value
+		const i = state[matchesKey.value].indexOf(name)
+
+		if (i !== -1) {
+			state[matchesKey.value].splice(i, 1)
+		} else {
+			state[matchesKey.value].push(name)
+		}
+
+		return refine(options)
 	}
 
 	function isFiltering(name?: string): boolean {
@@ -183,8 +186,25 @@ export function useRefinements<
 		return currentFilters.value.length !== 0
 	}
 
+	function isSorting(name?: string, direction?: SortDirection): boolean {
+		if (name) {
+			return refinements.value.sorts.some((sort) => sort.name === name && (direction ? sort.direction === direction : true))
+		}
+
+		return !!currentSort.value
+	}
+
+	function isSearching(name?: string): boolean {
+		if (name) {
+			return currentSearches.value.some((search) => search.name === name)
+		}
+
+		return refinements.value.search.value !== ''
+	}
+
 	function clearFilters(...filters: (string) []) {
 		if (filters.length === 0) {
+
 			for (const key in state) {
 				state[key] = undefined
 			}
@@ -197,17 +217,23 @@ export function useRefinements<
 		return refine()
 	}
 
-
-	function clearSort() {
+	function clearSort(options: VisitOptions = {}) {
 		state[sortsKey.value] = undefined
-		return refine()
+
+		return refine(options)
 	}
 
-	function clearSearch() {
+	function clearSearch(options: VisitOptions = {}) {
 		state[searchKey.value] = ''
-		return refine()
+
+		return refine(options)
 	}
 
+	function clearMatches(options: VisitOptions = {}) {
+		state[matchesKey.value] = []
+
+		return refine(options)
+	}
 
 	function reset(options: VisitOptions = {}) {
 		for (const key in state) {
@@ -217,12 +243,13 @@ export function useRefinements<
 		return refine(options)
 	}
 
+	updateState()
 
 	return {
+		state,
 		filters: computed(() => refinements.value.filters.map((filter) => ({
 			...filter,
-			// bind: (value: any) => state[filter.name] = value,
-			apply: (v: any) => applyFilter(filter.name, v),
+			apply: (v: FilterBinding<typeof filter>) => applyFilter(filter.name, v),
 			clear: () => clearFilters(filter.name),
 		}))),
 		sorts: computed(() => refinements.value.sorts.map((sort) => ({
@@ -231,20 +258,26 @@ export function useRefinements<
 		}))),
 		searches: computed(() => refinements.value.searches.map((search) => ({
 			...search,
-			// bind: (value: string) => baseSearch.value = value,
-			toggle: () => applySearch(search.name),
+			toggle: () => applyMatch(search.name),
 		}))),
+		currentSort,
+		currentFilters,
+		currentSearches,
+		refine,
 		getFilter,
 		getSort,
+		getMatch,
 		applyFilter,
 		applySort,
 		applySearch,
-		isSorting,
+		applyMatch,
 		isFiltering,
+		isSorting,
+		isSearching,
 		clearFilters,
 		clearSort,
 		clearSearch,
+		clearMatches,
 		reset,
-		refine,
 	}
 }
