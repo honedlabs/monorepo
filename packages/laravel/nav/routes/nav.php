bde5b607e2b00d47161232e29885bf1c080577a6
jdw5 <joshuadwallace1@gmail.com>
2025-02-01 18:36:57 +1000

    feat(nav): route file for publishing

---
+++ packages/laravel/nav/routes/nav.php
@@ -0,0 +1,7 @@
+<?php
+
+declare(strict_types=1);
+
+use Honed\Nav\Facades\Nav;
+
+// Nav::make('app', );
--- packages/laravel/nav/src/NavServiceProvider.php
+++ packages/laravel/nav/src/NavServiceProvider.php
@@ -25,6 +25,10 @@ class NavServiceProvider extends ServiceProvider
             __DIR__.'/../config/nav.php' => config_path('nav.php'),
         ], 'nav-config');
 
+        $this->publishes([
+            __DIR__.'/../routes/nav.php' => base_path('routes/nav.php'),
+        ], 'nav-routes');
+
         $this->registerNavigation();
     }
 
