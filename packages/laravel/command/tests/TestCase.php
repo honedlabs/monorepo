a75d3e60590e9aa191bbcf674f4f62e9d21ee50b
jdw5 <joshuadwallace1@gmail.com>
2025-02-25 13:07:40 +1000

    fix(command): removed inertia dependencies

--- packages/laravel/command/tests/TestCase.php
+++ packages/laravel/command/tests/TestCase.php
@@ -8,29 +8,14 @@ use Honed\Command\CommandServiceProvider;
 use Honed\Command\Tests\Stubs\Status;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-use Illuminate\Support\Facades\View;
-use Inertia\Inertia;
-use Inertia\ServiceProvider as InertiaServiceProvider;
 use Orchestra\Testbench\TestCase as Orchestra;
 
 class TestCase extends Orchestra
 {
-    protected function setUp(): void
-    {
-        parent::setUp();
-
-        View::addLocation(__DIR__.'/Stubs');
-        Inertia::setRootView('app');
-        config()->set('inertia.testing.ensure_pages_exist', false);
-        config()->set('inertia.testing.page_paths', [realpath(__DIR__)]);
-
-    }
-
     protected function getPackageProviders($app)
     {
         return [
             CommandServiceProvider::class,
-            InertiaServiceProvider::class,
         ];
     }
 
@@ -46,11 +31,6 @@ class TestCase extends Orchestra
             $table->boolean('best_seller')->default(false);
             $table->timestamps();
         });
-    }
-
-    protected function defineRoutes($router)
-    {
-        // $router->get('/', fn () => 'Hello World');
     }
 
     public function getEnvironmentSetUp($app)
