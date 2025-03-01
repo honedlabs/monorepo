6d3504c38b94bc82d21e5e8dd7c23e0dcb457c11
jdw5 <joshuadwallace1@gmail.com>
2025-03-01 23:34:26 +1000

    test(nav): reintroduced nav item tests

--- packages/laravel/nav/tests/Pest/NavItemTest.php
+++ packages/laravel/nav/tests/Pest/NavItemTest.php
@@ -12,19 +12,19 @@ beforeEach(function () {
     $this->label = 'Home';
 });
 
-// it('makes', function () {
-//     get(route('products.index'));
-//     expect(NavItem::make($this->label, 'products.index'))
-//         ->toBeInstanceOf(NavItem::class)
-//         ->getLabel()->toBe($this->label)
-//         ->getRoute()->toBe(route('products.index'))
-//         ->toArray()->toEqual([
-//             'label' => $this->label,
-//             'href' => route('products.index'),
-//             'active' => true,
-//             'icon' => null,
-//         ]);
-// });
+it('makes', function () {
+    get(route('products.index'));
+    expect(NavItem::make($this->label, 'products.index'))
+        ->toBeInstanceOf(NavItem::class)
+        ->getLabel()->toBe($this->label)
+        ->getRoute()->toBe(route('products.index'))
+        ->toArray()->toEqual([
+            'label' => $this->label,
+            'href' => route('products.index'),
+            'active' => true,
+            'icon' => null,
+        ]);
+});
 
 it('sets active state', function (string|\Closure|null $condition, bool $expected) {
     $product = product();
@@ -43,12 +43,12 @@ it('sets active state', function (string|\Closure|null $condition, bool $expecte
             'icon' => null,
         ]);
 })->with([
-    // 'other route' => ['status.*', false],
-    // 'all' => ['*', true],
-    // 'item route' => [null, true],
-    // 'wildcard' => ['products.*', true],
+    'other route' => ['status.*', false],
+    'all' => ['*', true],
+    'item route' => [null, true],
+    'wildcard' => ['products.*', true],
     'typed parameter product' => fn () => [fn (Product $p) => request()->url() === route('products.show', $p), true],
-    // 'named parameter product' => fn () => [fn ($product) => request()->url() === route('products.show', $product), true],
-    // 'typed parameter route' => fn () => [fn (Route $r) => $r->getName() === 'products.show', true],
-    // 'named parameter route' => fn () => [fn ($route) => $route->getName() === 'products.show', true],
+    'named parameter product' => fn () => [fn ($product) => request()->url() === route('products.show', $product), true],
+    'typed parameter route' => fn () => [fn (Route $r) => $r->getName() === 'products.show', true],
+    'named parameter route' => fn () => [fn ($route) => $route->getName() === 'products.show', true],
 ]);
