07e0eff725e97c892e69769efb80f9a23949059c
jdw5 <joshuadwallace1@gmail.com>
2025-03-03 23:20:59 +1000

    feat(flash): helper function

--- packages/laravel/flash/_ide_helpers.php
+++ packages/laravel/flash/_ide_helpers.php
@@ -4,22 +4,22 @@ declare(strict_types=1);
 
 namespace Illuminate\Http {
     /**
-     * @method \Illuminate\Http\RedirectResponse flash(string|\Honed\Flash\Message $message, string|null $type = null, string|null $title = null, int|null $duration = null, array $meta = []) Flash a new message to the session.
-     * @method \Illuminate\Http\RedirectResponse success(string $message, string|null $title = null, int|null $duration = null, array $meta = []) Flash a new success message to the session.
-     * @method \Illuminate\Http\RedirectResponse error(string $message, string|null $title = null, int|null $duration = null, array $meta = []) Flash a new error message to the session.
-     * @method \Illuminate\Http\RedirectResponse info(string $message, string|null $title = null, int|null $duration = null, array $meta = []) Flash a new info message to the session.
-     * @method \Illuminate\Http\RedirectResponse warning(string $message, string|null $title = null, int|null $duration = null, array $meta = []) Flash a new warning message to the session.
+     * @method $this flash(string|\Honed\Flash\Message $message, string|null $type = null, string|null $title = null, int|null $duration = null, array $meta = []) Flash a new message to the session.
+     * @method $this success(string $message, string|null $title = null, int|null $duration = null, array $meta = []) Flash a new success message to the session.
+     * @method $this error(string $message, string|null $title = null, int|null $duration = null, array $meta = []) Flash a new error message to the session.
+     * @method $this info(string $message, string|null $title = null, int|null $duration = null, array $meta = []) Flash a new info message to the session.
+     * @method $this warning(string $message, string|null $title = null, int|null $duration = null, array $meta = []) Flash a new warning message to the session.
      */
     class RedirectResponse {}
 }
 
 namespace Inertia {
     /**
-     * @method \Inertia\ResponseFactory flash(string|\Honed\Flash\Message $message, string|null $type = null, string|null $title = null, int|null $duration = null, array $meta = []) Flash a new message to the session.
-     * @method \Inertia\ResponseFactory success(string $message, string|null $title = null, int|null $duration = null, array $meta = []) Flash a new success message to the session.
-     * @method \Inertia\ResponseFactory error(string $message, string|null $title = null, int|null $duration = null, array $meta = []) Flash a new error message to the session.
-     * @method \Inertia\ResponseFactory info(string $message, string|null $title = null, int|null $duration = null, array $meta = []) Flash a new info message to the session.
-     * @method \Inertia\ResponseFactory warning(string $message, string|null $title = null, int|null $duration = null, array $meta = []) Flash a new warning message to the session.
+     * @method $this flash(string|\Honed\Flash\Message $message, string|null $type = null, string|null $title = null, int|null $duration = null, array $meta = []) Flash a new message to the session.
+     * @method $this success(string $message, string|null $title = null, int|null $duration = null, array $meta = []) Flash a new success message to the session.
+     * @method $this error(string $message, string|null $title = null, int|null $duration = null, array $meta = []) Flash a new error message to the session.
+     * @method $this info(string $message, string|null $title = null, int|null $duration = null, array $meta = []) Flash a new info message to the session.
+     * @method $this warning(string $message, string|null $title = null, int|null $duration = null, array $meta = []) Flash a new warning message to the session.
      */
     class ResponseFactory {}
 }
--- packages/laravel/flash/helpers.php
+++ packages/laravel/flash/helpers.php
@@ -3,12 +3,28 @@
 declare(strict_types=1);
 
 if (!function_exists('flash')) {
-    function flash($component = null, $props = [])
-    {
+    /**
+     * Flash a new message to the session.
+     * 
+     * @param string|\Honed\Flash\Message|null $message
+     * @param string|null $type
+     * @param string|null $title
+     * @param int|null $duration
+     * @param array<string,mixed> $meta
+     * @return \Honed\Flash\Flash
+     */
+    function flash(
+        $message = null,
+        $type = null,
+        $title = null,
+        $duration = null,
+        $meta = []
+    ) {
+        /** @var \Honed\Flash\Flash $instance */
         $instance = \Honed\Flash\Facades\Flash::getFacadeRoot();
 
-        if ($component) {
-            return $instance->render($component, $props);
+        if ($message) {
+            return $instance->message(...\func_get_args());
         }
 
         return $instance;
