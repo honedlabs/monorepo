6f67f698c21f298fc3c5ffa468f46dba9197cf72
jdw5 <joshuadwallace1@gmail.com>
2025-03-04 20:36:42 +1000

    feat(flash): restructured flash funcs

--- packages/vue/flash/src/index.ts
+++ packages/vue/flash/src/index.ts
@@ -1,3 +1,3 @@
-export { flash as useFlash, success, error, info, warning } from './flash'
-export { plugin as flash, type FlashPluginOptions } from './plugin'
+export { useFlash, flash, success, error, info, warning } from './flash'
+export { plugin as flashing, type FlashPluginOptions } from './plugin'
 export type { Message } from './inertia'
