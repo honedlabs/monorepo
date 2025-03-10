dbad783e6568a4fb5f894e01c9450406204001b3
jdw5 <joshuadwallace1@gmail.com>
2025-03-10 12:50:34 +1000

    feat(action): updated commands

--- packages/laravel/action/src/Console/Commands/ActionMakeCommand.php
+++ packages/laravel/action/src/Console/Commands/ActionMakeCommand.php
@@ -4,9 +4,16 @@ declare(strict_types=1);
 
 namespace Honed\Action\Console\Commands;
 
+use Illuminate\Support\Str;
+use function Laravel\Prompts\error;
+use function Laravel\Prompts\select;
+use function Laravel\Prompts\suggest;
 use Illuminate\Console\GeneratorCommand;
-use Symfony\Component\Console\Attribute\AsCommand;
+
 use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
 
 #[AsCommand(name: 'make:action')]
 class ActionMakeCommand extends GeneratorCommand
@@ -23,7 +30,7 @@ class ActionMakeCommand extends GeneratorCommand
      *
      * @var string
      */
-    protected $description = 'Create a new atomic action class';
+    protected $description = 'Create a new actionable class.';
 
     /**
      * The type of class being generated.
@@ -33,13 +40,111 @@ class ActionMakeCommand extends GeneratorCommand
     protected $type = 'Action';
 
     /**
+     * The actions that can be used in the action.
+     *
+     * @var array<string,string>
+     */
+    protected $actions = [
+        'create' => 'Create',
+        'delete' => 'Delete',
+        'update' => 'Update',
+        'show' => 'Show',
+        'index' => 'Index',
+        'store' => 'Store',
+        'destroy' => 'Destroy',
+    ];
+
+    /**
+     * Build the class with the given name.
+     *
+     * @param  string  $name
+     * @return string
+     */
+    protected function buildClass($name)
+    {
+        $stub = parent::buildClass($name);
+
+        $model = $this->option('model');
+        $action = $this->option('action');
+
+        if ($action && ! $model) {
+            error('You must provide a model when specifying the action type.');
+        }
+
+        return $model
+            ? $this->replaceModel($stub, $model) 
+            : $stub;
+    }
+
+    /**
+     * Replace the model for the given stub.
+     *
+     * @param  string  $stub
+     * @param  string  $model
+     * @return string
+     */
+    protected function replaceModel($stub, $model)
+    {
+        $model = str_replace('/', '\\', $model);
+
+        if (str_starts_with($model, '\\')) {
+            $namespacedModel = trim($model, '\\');
+        } else {
+            $namespacedModel = $this->qualifyModel($model);
+        }
+
+        $model = class_basename(trim($model, '\\'));
+
+        $dummyUser = class_basename($this->userProviderModel());
+
+        $dummyModel = Str::camel($model) === 'user' ? 'model' : $model;
+
+        $replace = [
+            'NamespacedDummyModel' => $namespacedModel,
+            '{{ namespacedModel }}' => $namespacedModel,
+            '{{namespacedModel}}' => $namespacedModel,
+            'DummyModel' => $model,
+            '{{ model }}' => $model,
+            '{{model}}' => $model,
+            'dummyModel' => Str::camel($dummyModel),
+            '{{ modelVariable }}' => Str::camel($dummyModel),
+            '{{modelVariable}}' => Str::camel($dummyModel),
+            'DummyUser' => $dummyUser,
+            '{{ user }}' => $dummyUser,
+            '{{user}}' => $dummyUser,
+            '$user' => '$'.Str::camel($dummyUser),
+        ];
+
+        $stub = str_replace(
+            array_keys($replace), array_values($replace), $stub
+        );
+
+        $contract = "use Honed\Action\Contracts\Actionable;\r\n";
+
+        return preg_replace(
+            '/' . preg_quote($contract, '/') . '/',
+            vsprintf("use %s;\r\nuse %s;\r\n", [
+                'Honed\Action\Contracts\Actionable',
+                $namespacedModel,
+            ]),
+            $stub
+        );
+    }
+
+    /**
      * Get the stub file for the generator.
      *
      * @return string
      */
     protected function getStub()
     {
-        return $this->resolveStubPath('/stubs/action.stub');
+        $stub = str($this->option('action'))
+            ->prepend('/stubs/honed.action.')
+            ->append('.stub')
+            ->replace('..', '.')
+            ->value();
+            
+        return $this->resolveStubPath($stub);
     }
 
     /**
@@ -74,7 +179,49 @@ class ActionMakeCommand extends GeneratorCommand
     protected function getOptions()
     {
         return [
-            ['force', null, InputOption::VALUE_NONE, 'Create the class even if the action already exists'],
+            ['force', null, InputOption::VALUE_NONE, 'Create the class even if the action already exists.'],
+            ['model', 'm', InputOption::VALUE_REQUIRED, 'The model that the action is for.'],
+            ['action', 'a', InputOption::VALUE_REQUIRED, 'The action to be used.'],
         ];
+    }
+
+    /**
+     * Interact further with the user if they were prompted for missing arguments.
+     *
+     * @param  \Symfony\Component\Console\Input\InputInterface  $input
+     * @param  \Symfony\Component\Console\Output\OutputInterface  $output
+     * @return void
+     */
+    protected function afterPromptingForMissingArguments(InputInterface $input, OutputInterface $output)
+    {
+        if ($this->isReservedName($this->getNameInput()) || $this->didReceiveOptions($input)) {
+            return;
+        }
+
+        $actions = \array_merge($this->actions, [
+            'none' => 'None',
+        ]);
+
+        $action = select(
+            'What action should be used? (Optional)',
+            $actions,
+            scroll: \count($actions),
+            hint: 'If no action is provided, the default action stub will be used.',
+        );
+
+        if ($action === 'none') {
+            return;
+        }
+
+        // If a action is selected, they must also provide a model.
+        $input->setOption('action', $action);
+
+        $model = suggest(
+            'What model should this action be for? (Optional)',
+            $this->possibleModels(),
+            required: 'This field is required when an action is selected',
+        );
+
+        $input->setOption('model', $model);
     }
 }
--- packages/laravel/action/tests/Pest/CommandTest.php
+++ packages/laravel/action/tests/Pest/CommandTest.php
@@ -2,10 +2,36 @@
 
 declare(strict_types=1);
 
+use Illuminate\Support\Facades\File;
+
+beforeEach(function () {
+    File::cleanDirectory(app_path('Actions'));
+});
+
 it('tests', function () {
     $this->artisan('make:action', [
         'name' => 'TestAction',
     ])->assertSuccessful();
 
     $this->assertFileExists(app_path('Actions/TestAction.php'));
+});
+
+it('sets verb', function () {
+    $this->artisan('make:action', [
+        'name' => 'UpdateAction',
+        '--action' => 'update',
+        '--model' => 'Product',
+    ])->assertSuccessful();
+
+    $this->assertFileExists(app_path('Actions/UpdateAction.php'));
+});
+
+it('prompts', function () {
+    $this->artisan('make:action')
+        ->expectsQuestion('What should the action be named?', 'StoreAction')
+        ->expectsQuestion('What action should be used? (Optional)', 'store')
+        ->expectsQuestion('What model should this action be for? (Optional)', 'Product')
+        ->assertSuccessful();
+
+    $this->assertFileExists(app_path('Actions/StoreAction.php'));
 });
