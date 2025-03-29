1047c3d8d3156e4b01f3fa4112b1389b91cf5c82
jdw5 <joshuadwallace1@gmail.com>
2025-03-29 14:42:04 +1000

    chore(nav): bump core

--- packages/laravel/nav/src/Facades/Nav.php
+++ packages/laravel/nav/src/Facades/Nav.php
@@ -32,8 +32,10 @@ class Nav extends Facade
 
     /**
      * Get the registered name of the component.
+     *
+     * @return string
      */
-    protected static function getFacadeAccessor(): string
+    protected static function getFacadeAccessor()
     {
         return NavFactory::class;
     }
