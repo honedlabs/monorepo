807f6531da401a94b1c770df6b3d96387e865e71
jdw5 <joshuadwallace1@gmail.com>
2025-04-10 16:34:17 +1000

    feat(layout): initial version for trialling layout

--- packages/vue/layout/dist/index.d.ts
+++ packages/vue/layout/dist/index.d.ts
@@ -1,54 +1,17 @@
-import { App } from 'vue';
+import { createInertiaApp as createInertiaApp_2 } from '@inertiajs/vue3';
 import { DefineComponent } from 'vue';
-import { Page } from '@inertiajs/core';
-import { Plugin as Plugin_2 } from 'vue';
 
-declare function createInertiaApp({ id, resolve, layout: layoutResolver, setup, title, progress, page, render, }: CreateInertiaAppProps): Promise<{
+export declare function createInertiaApp(props: CreateInertiaAppProps): Promise<{
     head: string[];
     body: string;
-} | void>;
-export default createInertiaApp;
+}>;
 
-export declare interface CreateInertiaAppProps {
-    id?: string;
-    resolve: (name: string) => DefineComponent | Promise<DefineComponent> | {
+declare type CreateInertiaAppParameters = Parameters<typeof createInertiaApp_2>[0];
+
+export declare interface CreateInertiaAppProps extends CreateInertiaAppParameters {
+    layout: (name: string | null) => DefineComponent | Promise<DefineComponent> | {
         default: DefineComponent;
     };
-    layout: (layout: string) => DefineComponent | Promise<DefineComponent> | {
-        default: DefineComponent;
-    };
-    setup: (props: {
-        el: Element;
-        App: InertiaApp;
-        props: InertiaAppProps;
-        plugin: Plugin_2;
-    }) => void | App;
-    title?: (title: string) => string;
-    progress?: false | {
-        delay?: number;
-        color?: string;
-        includeCSS?: boolean;
-        showSpinner?: boolean;
-    };
-    page?: Page;
-    render?: (app: App) => Promise<string>;
-}
-
-declare type InertiaApp = DefineComponent<InertiaAppProps>;
-
-declare interface InertiaAppProps {
-    initialPage: Page;
-    initialComponent?: object;
-    resolveComponent?: (name: string) => DefineComponent | Promise<DefineComponent>;
-    titleCallback?: (title: string) => string;
-    onHeadUpdate?: (elements: string[]) => void;
 }
 
 export { }
-
-
-declare module "@inertiajs/vue3" {
-    interface PageProps {
-        layout: string | null;
-    }
-}
--- packages/vue/layout/src/index.ts
+++ packages/vue/layout/src/index.ts
@@ -1,113 +1,33 @@
-import { Page, router, setupProgress } from "@inertiajs/core";
-import { DefineComponent, Plugin, App as VueApp, createSSRApp, h } from "vue";
-import App, { InertiaApp, InertiaAppProps, plugin } from "./app";
+import { DefineComponent } from "vue";
+import { createInertiaApp as inertiaApp } from "@inertiajs/vue3";
 
-interface InertiaAppProps {
-	initialPage: Page;
-	initialComponent?: object;
-	resolveComponent?: (
-		name: string,
-	) => DefineComponent | Promise<DefineComponent>;
-	titleCallback?: (title: string) => string;
-	onHeadUpdate?: (elements: string[]) => void;
-}
+type CreateInertiaAppParameters = Parameters<typeof inertiaApp>[0];
 
-type InertiaApp = DefineComponent<InertiaAppProps>;
-
-export interface CreateInertiaAppProps {
-	id?: string;
-	resolve: (
-		name: string,
-	) =>
-		| DefineComponent
-		| Promise<DefineComponent>
-		| { default: DefineComponent };
+export interface CreateInertiaAppProps extends CreateInertiaAppParameters {
 	layout: (
-		layout: string,
+		name: string | null,
 	) =>
 		| DefineComponent
 		| Promise<DefineComponent>
 		| { default: DefineComponent };
-	setup: (props: {
-		el: Element;
-		App: InertiaApp;
-		props: InertiaAppProps;
-		plugin: Plugin;
-	}) => void | VueApp;
-	title?: (title: string) => string;
-	progress?:
-		| false
-		| {
-				delay?: number;
-				color?: string;
-				includeCSS?: boolean;
-				showSpinner?: boolean;
-		  };
-	page?: Page;
-	render?: (app: VueApp) => Promise<string>;
 }
 
-export default async function createInertiaApp({
-	id = "app",
-	resolve,
-	layout: layoutResolver,
-	setup,
-	title,
-	progress = {},
-	page,
-	render,
-}: CreateInertiaAppProps): Promise<{ head: string[]; body: string } | void> {
+export async function createInertiaApp(props: CreateInertiaAppProps) {
+	const { id = "app", resolve, layout, ...rest } = props;
+
 	const isServer = typeof window === "undefined";
 	const el = isServer ? null : document.getElementById(id);
-	const initialPage = page || JSON.parse(el!.dataset.page as string);
-	const initialLayout = JSON.parse(el!.dataset.layout as string);
+	const initialLayout = JSON.parse(el?.dataset.layout || "{}");
 
-	const resolveComponent = async (page: string, layout: string | null) => {
-		const pageComponent = await resolve(page);
-		const layoutComponent = layout ? await layoutResolver(layout) : null;
-		pageComponent.default.layout =
-			pageComponent.default.layout || layoutComponent;
-		return pageComponent;
+	const layoutResolve = async (name: string) => {
+		const page = await resolve(name);
+
+		const pageLayout = await layout(initialLayout);
+
+		page.default.layout = page.default.layout || pageLayout;
+
+		return page.default;
 	};
 
-	let head: string[] = [];
-
-	const vueApp = await Promise.all([
-		resolveComponent(initialPage.component, initialLayout.component),
-		router.decryptHistory().catch(() => {}),
-	]).then(([initialComponent]) => {
-		return setup({
-			el: el as Element,
-			App,
-			props: {
-				initialPage,
-				initialComponent,
-				resolveComponent,
-				titleCallback: title,
-				onHeadUpdate: isServer
-					? (elements: string[]) => (head = elements)
-					: null,
-			},
-			plugin,
-		});
-	});
-
-	if (!isServer && progress) {
-		setupProgress(progress);
-	}
-
-	if (isServer && render) {
-		const body = await render(
-			createSSRApp({
-				render: () =>
-					h("div", {
-						id,
-						"data-page": JSON.stringify(initialPage),
-						innerHTML: vueApp ? render(vueApp) : "",
-					}),
-			}),
-		);
-
-		return { head, body };
-	}
+	return inertiaApp({ ...rest, id, resolve: layoutResolve });
 }
