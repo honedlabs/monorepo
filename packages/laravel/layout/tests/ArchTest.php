783b36ec934c8b63361946eb731b769d9d97a3f6
jdw5 <joshuadwallace1@gmail.com>
2025-04-11 14:38:12 +1000

    feat(layout): arch tests, autoloading of enum_value func

--- packages/laravel/layout/tests/ArchTest.php
+++ packages/laravel/layout/tests/ArchTest.php
@@ -5,3 +5,11 @@ declare(strict_types=1);
 arch('it will not use debugging functions')
     ->expect(['dd', 'dump', 'ray'])
     ->each->not->toBeUsed();
+
+arch()->preset()->php();
+
+arch()->preset()->security();
+
+arch('strict types')
+    ->expect('Honed\Layout')
+    ->toUseStrictTypes();
