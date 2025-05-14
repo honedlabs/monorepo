477ebb46f665983aa031117db0f8f0c9598da799
jdw5 <joshuadwallace1@gmail.com>
2025-05-15 08:53:17 +1000

    feat(widget): templating out database driver

--- packages/laravel/widget/src/Drivers/DatabaseDriver.php
+++ packages/laravel/widget/src/Drivers/DatabaseDriver.php
@@ -5,8 +5,93 @@ declare(strict_types=1);
 namespace Honed\Widget\Drivers;
 
 use Honed\Widget\Contracts\Driver;
+use Illuminate\Config\Repository;
+use Illuminate\Contracts\Events\Dispatcher;
+use Illuminate\Database\Connection;
+use Illuminate\Database\DatabaseManager;
 
 class DatabaseDriver implements Driver
 {
-    
+    /**
+     * The database connection.
+     *
+     * @var \Illuminate\Database\DatabaseManager
+     */
+    protected $db;
+
+    /**
+     * The user configuration.
+     *
+     * @var \Illuminate\Config\Repository
+     */
+    protected $config;
+
+    /**
+     * The store's name.
+     *
+     * @var string
+     */
+    protected $name;
+
+    /**
+     * The event dispatcher.
+     *
+     * @var \Illuminate\Contracts\Events\Dispatcher
+     */
+    protected $events;
+
+    /**
+     * The name of the "created at" column.
+     *
+     * @var string
+     */
+    const CREATED_AT = 'created_at';
+
+    /**
+     * The name of the "updated at" column.
+     *
+     * @var string
+     */
+    const UPDATED_AT = 'updated_at';
+
+    /**
+     * Create a new driver instance.
+     *
+     * @return void
+     */
+    public function __construct(
+        DatabaseManager $db,
+        Dispatcher $events,
+        Repository $config,
+        string $name
+    ) {
+        $this->db = $db;
+        $this->events = $events;
+        $this->config = $config;
+        $this->name = $name;
+    }
+
+    /**
+     * Create a new table query.
+     *
+     * @return \Illuminate\Database\Query\Builder
+     */
+    protected function newQuery()
+    {
+        return $this->connection()->table(
+            $this->config->get("widget.drivers.{$this->name}.table") ?? 'widgets'
+        );
+    }
+
+    /**
+     * The database connection.
+     *
+     * @return \Illuminate\Database\Connection
+     */
+    protected function connection()
+    {
+        return $this->db->connection(
+            $this->config->get("widget.drivers.{$this->name}.connection") ?? null
+        );
+    }    
 }
--- packages/laravel/widget/src/Drivers/Decorator.php
+++ packages/laravel/widget/src/Drivers/Decorator.php
@@ -5,6 +5,9 @@ declare(strict_types=1);
 namespace Honed\Widget\Drivers;
 
 use Honed\Widget\Contracts\Driver;
+use Honed\Widget\Events\WidgetDeleted;
+use Honed\Widget\Events\WidgetUpdated;
+use Illuminate\Support\Facades\Event;
 use Illuminate\Support\Traits\Macroable;
 
 class Decorator implements Driver
@@ -12,7 +15,6 @@ class Decorator implements Driver
     use Macroable {
         __call as macroCall;
     }
-
 
     /**
      * The driver name.
@@ -35,7 +37,6 @@ class Decorator implements Driver
      * @var callable(): mixed
      */
     protected $defaultScopeResolver;
-
 
     /**
      * The container instance.
@@ -69,31 +70,100 @@ class Decorator implements Driver
      */
     public function get(string $widget, string $scope): mixed
     {
+        $widget = $this->resolveWidget($widget);
 
+        $scope = $this->resolveScope($scope);
+
+        $item = null;
+
+        // Event::dispatch(new WidgetRetrieved($widget, $scope, $item));
+
+        return $item;
     }
 
     public function set(string $widget, string $scope, mixed $value): void
     {
+        $widget = $this->resolveWidget($widget);
+
+        $scope = $this->resolveScope($scope);
+
+        $this->driver->set($widget, $scope, $value);
+
+        Event::dispatch(new WidgetUpdated($widget, $scope));
 
     }
 
     public function delete(string $widget, string $scope): void
     {
+        $widget = $this->resolveWidget($widget);
+
+        $scope = $this->resolveScope($scope);
+
+        $this->driver->delete($widget, $scope);
+
+        Event::dispatch(new WidgetDeleted($widget, $scope));
 
     }
 
+    /**
+     * Retrieve the widget's name.
+     * 
+     * @param string $widget
+     * @return string
+     */
     public function name($widget)
     {
-
+        return $this->resolveWidget($widget);
     }
-
+    
+    /**
+     * Retrieve the widget's class.
+     * 
+     * @param string $name
+     * @return \Honed\Widget\Contracts\Widget
+     */
     public function instance($name)
     {
-
+        $this->container->make($name);
     }
 
+    /**
+     * Get the underlying driver instance.
+     * 
+     * @return \Honed\Widget\Contracts\Driver
+     */
     public function getDriver()
     {
+        return $this->driver;
+    }
 
+    /**
+     * Set the container instance used by the decorator.
+     *
+     * @param \Illuminate\Contracts\Container\Container $container
+     * @return $this
+     */
+    public function setContainer($container)
+    {
+        $this->container = $container;
+
+        return $this;
+    }
+
+
+    /**
+     * Dynamically create a pending feature interaction.
+     *
+     * @param  string  $name
+     * @param  array<mixed>  $parameters
+     * @return mixed
+     */
+    public function __call($name, $parameters)
+    {
+        if (static::hasMacro($name)) {
+            return $this->macroCall($name, $parameters);
+        }
+
+        // Forward to the driver
     }
 }
