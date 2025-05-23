7a561369d4b998dc7700bb2285bd7e8d0b30b56a
jdw5 <joshuadwallace1@gmail.com>
2025-05-23 15:19:25 +1000

    feat(modal): workbench

--- packages/laravel/modal/config/modal.php
+++ packages/laravel/modal/config/modal.php
@@ -8,10 +8,10 @@ return [
     | Fortify Routes Middleware
     |--------------------------------------------------------------------------
     |
-    | If necessary, you may add
-    | middleware to be executed whenever a modal response is needed to be 
-    | returned. Typically this should be done at the routing or controller
-    | level, but you can provide modal middleware if necessary
+    | If necessary, you may add middleware to be executed whenever a modal
+    | response is needed to be returned. Typically this should be done at the
+    | routing or controller level, but you can provide modal middleware if
+    | necessary.
     |
     */
 
