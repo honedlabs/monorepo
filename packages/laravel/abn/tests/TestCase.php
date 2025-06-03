1e925d4399ecaf60f0231cc436a4bfbbdfde3001
jdw5 <joshuadwallace1@gmail.com>
2025-06-03 12:31:12 +1000

    fix(abn): added migration attribute

--- packages/laravel/abn/tests/TestCase.php
+++ packages/laravel/abn/tests/TestCase.php
@@ -5,9 +5,11 @@ declare(strict_types=1);
 namespace Honed\Abn\Tests;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Orchestra\Testbench\Attributes\WithMigration;
 use Orchestra\Testbench\Concerns\WithWorkbench;
 use Orchestra\Testbench\TestCase as Orchestra;
 
+#[WithMigration('laravel')]
 class TestCase extends Orchestra
 {
     use RefreshDatabase;
