7cefe3546d3d811d9ab98de08731c026ce3858f5
jdw5 <joshuadwallace1@gmail.com>
2025-06-03 12:33:44 +1000

    fix(abn): db migrations in testcase

--- packages/laravel/abn/tests/TestCase.php
+++ packages/laravel/abn/tests/TestCase.php
@@ -5,13 +5,24 @@ declare(strict_types=1);
 namespace Honed\Abn\Tests;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Orchestra\Testbench\Attributes\WithMigration;
 use Orchestra\Testbench\Concerns\WithWorkbench;
 use Orchestra\Testbench\TestCase as Orchestra;
 
-#[WithMigration('laravel')]
+use function Orchestra\Testbench\workbench_path;
+
 class TestCase extends Orchestra
 {
     use RefreshDatabase;
     use WithWorkbench;
-}
+
+    /**
+     * Define database migrations.
+     *
+     * @return void
+     */
+    protected function defineDatabaseMigrations() 
+    {
+        $this->loadMigrationsFrom(
+            workbench_path('database/migrations')
+        );
+    }}
