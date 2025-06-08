7a1eca151db365b3abf91ae4a543060f10a08bc3
jdw5 <joshuadwallace1@gmail.com>
2025-06-08 15:44:23 +1000

    fix(flash): added inertia service provider

--- packages/laravel/flash/workbench/bootstrap/providers.php
+++ packages/laravel/flash/workbench/bootstrap/providers.php
@@ -4,5 +4,6 @@ declare(strict_types=1);
 
 return [
     Honed\Flash\FlashServiceProvider::class,
+    Inertia\ServiceProvider::class,
     Workbench\App\Providers\WorkbenchServiceProvider::class,
 ];
