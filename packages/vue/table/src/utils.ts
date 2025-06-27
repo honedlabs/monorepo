33c19130110fbb852bee0bc5741e95d387560981
jdw5 <joshuadwallace1@gmail.com>
2025-06-27 10:42:23 +1000

    feat(table): removed destructuring of refine

--- packages/vue/table/src/utils.ts
+++ packages/vue/table/src/utils.ts
@@ -1,5 +1,8 @@
 import type { Column, ColumnType } from "./types";
 
+/**
+ * Check if a column is of a given type.
+ */
 export function is(
 	column: Column | string | null | undefined,
 	type: ColumnType,
