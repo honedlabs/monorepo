b1a3f378d2c92e6c9a2d89db6c0d51ac035f29b4
jdw5 <joshuadwallace1@gmail.com>
2025-06-28 13:22:52 +1000

    feat(core): value normalization

--- packages/laravel/core/src/Concerns/HasValue.php
+++ packages/laravel/core/src/Concerns/HasValue.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace Honed\Core\Concerns;
 
+use Carbon\CarbonInterface;
+use Illuminate\Contracts\Support\Arrayable;
+
 trait HasValue
 {
     /**
@@ -34,5 +37,30 @@ trait HasValue
     public function getValue()
     {
         return $this->value;
+    }
+
+    /**
+     * Get the normalized value of the instance.
+     *
+     * @return mixed
+     */
+    public function getNormalizedValue()
+    {
+        return $this->normalizeValue($this->getValue());
+    }
+
+    /**
+     * Normalize the value of the instance.
+     *
+     * @param  mixed  $value
+     * @return mixed
+     */
+    public function normalizeValue($value)
+    {
+        return match (true) {
+            $value instanceof Arrayable => $value->toArray(),
+            $value instanceof CarbonInterface => $value->format('Y-m-d\TH:i:s'),
+            default => $value,
+        };
     }
 }
--- packages/laravel/core/tests/Feature/Concerns/HasValueTest.php
+++ packages/laravel/core/tests/Feature/Concerns/HasValueTest.php
@@ -3,6 +3,8 @@
 declare(strict_types=1);
 
 use Honed\Core\Concerns\HasValue;
+use Illuminate\Contracts\Support\Arrayable;
+use Illuminate\Support\Carbon;
 
 beforeEach(function () {
     $this->test = new class()
@@ -16,4 +18,26 @@ it('sets', function () {
         ->getValue()->toBeNull()
         ->value('test')->toBe($this->test)
         ->getValue()->toBe('test');
+});
+
+it('normalizes values', function () {
+    expect($this->test)
+        ->normalizeValue(Carbon::now())->toBe(Carbon::now()->format('Y-m-d\TH:i:s'))
+        ->normalizeValue(new class() implements Arrayable
+        {
+            public function toArray(): array
+            {
+                return ['test' => 'test'];
+            }
+        })->toBe(['test' => 'test'])
+        ->normalizeValue('test')->toBe('test');
+});
+
+it('gets normalized value', function () {
+    $this->test->value('test');
+
+    expect($this->test)
+        ->getNormalizedValue()->toBe('test')
+        ->value(Carbon::now())->toBe($this->test)
+        ->getNormalizedValue()->toBe(Carbon::now()->format('Y-m-d\TH:i:s'));
 });
