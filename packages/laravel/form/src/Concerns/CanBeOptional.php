ae5ada3d86e386e1bb83d4ef3026330708ee87ff
jdw5 <joshuadwallace1@gmail.com>
2025-08-21 19:34:34 +1000

    feat(form): optional and required traits

---
+++ packages/laravel/form/src/Concerns/CanBeOptional.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Honed\Form\Concerns;
+
+trait CanBeOptional
+{
+    /**
+     * Whether the component should be marked as optional.
+     * 
+     * @var bool
+     */
+    public bool $optional = false;
+
+    /**
+     * Set the optional state of the component.
+     * 
+     * @return $this
+     */
+    public function optional(bool $value = true): static
+    {
+        $this->optional = $value;
+
+        return $this;
+    }
+
+    /**
+     * Set the negated optional state of the component
+     * 
+     * @return $this
+     */
+    public function notOptional(bool $value = true): static
+    {
+        return $this->optional(! $value);
+    }
+
+    /**
+     * Get the optional state of the component.
+     */
+    public function isOptional(): bool
+    {
+        return $this->optional;
+    }
+    
+    /**
+     * Get the negated optional state of the component.
+     */
+    public function isNotOptional(): bool
+    {
+        return ! $this->isOptional();
+    }
+}
---
+++ packages/laravel/form/src/Concerns/CanBeRequired.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Honed\Form\Concerns;
+
+trait CanBeRequired
+{
+    /**
+     * Whether the component should be marked as required.
+     * 
+     * @var bool
+     */
+    public bool $required = false;
+
+    /**
+     * Set the required state of the component.
+     * 
+     * @return $this
+     */
+    public function required(bool $value = true): static
+    {
+        $this->required = $value;
+
+        return $this;
+    }
+
+    /**
+     * Set the negated required state of the component
+     * 
+     * @return $this
+     */
+    public function notRequired(bool $value = true): static
+    {
+        return $this->required(! $value);
+    }
+
+    /**
+     * Get the required state of the component.
+     */
+    public function isRequired(): bool
+    {
+        return $this->required;
+    }
+    
+    /**
+     * Get the negated required state of the component.
+     */
+    public function isNotRequired(): bool
+    {
+        return ! $this->isRequired();
+    }
+}
