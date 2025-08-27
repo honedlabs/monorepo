210438b2456dbbd89dc3a4e540c800e92f599210
jdw5 <joshuadwallace1@gmail.com>
2025-08-27 12:28:24 +1000

    feat(performance): gate

---
+++ packages/laravel/performance/src/Concerns/Memoizable.php
@@ -0,0 +1,75 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Honed\Performance\Concerns;
+
+/**
+ * @template T = mixed
+ */
+trait Memoizable
+{
+    /**
+     * The store of memoized values.
+     *
+     * @var array<string, T>
+     */
+    protected $memoize = [];
+
+    /**
+     * Set a memorized value.
+     * 
+     * @template TValue of T
+     * 
+     * @param TValue $value
+     * @return TValue
+     */
+    public function memoize(string $key, $value)
+    {
+        return $this->memoize[$key] = $value;
+    }
+
+    /**
+     * Get a memoized value.
+     * 
+     * @param string $key
+     * @return T
+     */
+    public function memoized(string $key)
+    {
+        return $this->memoize[$key];
+    }
+
+    /**
+     * Determine if a key is memorized.
+     */
+    public function isMemoized(string $key): bool
+    {
+        return isset($this->memoize[$key]);
+    }
+
+    /**
+     * Determine if a key is not memorized.
+     */
+    public function isNotMemoized(string $key): bool
+    {
+        return ! $this->isMemoized($key);
+    }
+
+
+    /**
+     * Clear the memoize cache.
+     */
+    public function clearMemoize(): void
+    {
+        $this->memoize = [];
+    }
+
+    /**
+     * Generate a hash for a given key.
+     */
+    public function getHash(mixed ...$values): string
+    {
+        return hash('md5', json_encode($values, JSON_THROW_ON_ERROR));
+    }
+}
--- packages/laravel/performance/src/PerformanceServiceProvider.php
+++ packages/laravel/performance/src/PerformanceServiceProvider.php
@@ -4,31 +4,34 @@ declare(strict_types=1);
 
 namespace Honed\Performance;
 
+use Illuminate\Contracts\Auth\Access\Gate as GateContract;
+use Illuminate\Contracts\Container\Container;
+use Illuminate\Contracts\Support\DeferrableProvider;
 use Illuminate\Support\ServiceProvider;
 
-class PerformanceServiceProvider extends ServiceProvider
+class PerformanceServiceProvider extends ServiceProvider implements DeferrableProvider
 {
     /**
      * Register services.
      */
     public function register(): void
     {
-        $this->mergeConfigFrom(__DIR__.'/../config/performance.php', 'performance');
+        $this->app->singleton(GateContract::class, function (Container $app) {
+            return new Gate($app, function () use ($app) {
+                return call_user_func($app['auth']->userResolver());
+            });
+        });
     }
 
     /**
-     * Bootstrap services.
+     * Get the services provided by the provider.
+     *
+     * @return array<int, class-string>
      */
-    public function boot(): void
+    public function provides(): array
     {
-        $this->publishes([
-            __DIR__.'/../config/performance.php' => config_path('performance.php'),
-        ], 'performance-config');
-
-        if ($this->app->runningInConsole()) {
-            $this->commands([
-                //
-            ]);
-        }
+        return [
+            GateContract::class,
+        ];
     }
 }
