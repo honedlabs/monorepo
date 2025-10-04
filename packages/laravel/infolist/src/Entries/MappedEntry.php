d0af417e30e4b575e9a9fccb0556bff0358ccd58
jdw5 <joshuadwallace1@gmail.com>
2025-10-04 10:30:30 +1000

    feat: mapped entry

---
+++ packages/laravel/infolist/src/Entries/MappedEntry.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Honed\Infolist\Entries;
+
+use BackedEnum;
+use Honed\Infolist\Formatters\MappedFormatter;
+
+/**
+ * @extends Entry<int|string|\BackedEnum, mixed>
+ */
+class MappedEntry extends Entry
+{
+    /**
+     * Provide the instance with any necessary setup.
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->formatter(MappedFormatter::class);
+    }
+}
---
+++ packages/laravel/infolist/src/Formatters/MappedFormatter.php
@@ -0,0 +1,91 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Honed\Infolist\Formatters;
+
+use Closure;
+use Honed\Infolist\Concerns\HasEnum;
+use Honed\Infolist\Contracts\Formatter;
+
+/**
+ * @implements Formatter<int|string|\BackedEnum, mixed>
+ */
+class MappedFormatter implements Formatter
+{
+    /**
+     * The mapping to use.
+     * 
+     * @var array<array-key, mixed>|\Closure(int|string|\BackedEnum|null):mixed
+     */
+    protected $mapping = [];
+
+    /**
+     * The default value to use if the value is not found in the mapping.
+     * 
+     * @var mixed
+     */
+    protected $default;
+    
+    /**
+     * Set the mapping to use.
+     * 
+     * @param array<array-key, mixed>|\Closure(int|string|\BackedEnum|null):mixed $value
+     * @return $this
+     */
+    public function mapping(array|Closure $value): static
+    {
+        $this->mapping = $value;
+
+        return $this;
+    }
+    
+    /**
+     * Get the mapping to use.
+     * 
+     * @return array<array-key, mixed>|\Closure(int|string|\BackedEnum|null):mixed
+     */
+    public function getMapping(): array|Closure
+    {
+        return $this->mapping;
+    }
+    
+    /**
+     * Set the default value to use if the value is not found in the mapping.
+     * 
+     * @return $this
+     */
+    public function default(mixed $value): static
+    {
+        $this->default = $value;
+
+        return $this;
+    }
+    
+    /**
+     * Get the default value to use if the value is not found in the mapping.
+     */
+    public function getDefault(): mixed
+    {
+        return $this->default;
+    }
+    
+    /**
+     * Format the value using a mapping.
+     * 
+     * @param int|string|\BackedEnum|null $value
+     * @return mixed
+     */
+    public function format(mixed $value): mixed
+    {
+        if (is_callable($this->mapping)) {
+            return ($this->mapping)($value) ?? $this->default;
+        }
+
+        if (isset($this->mapping[$value])) {
+            return $this->mapping[$value];
+        }
+
+        return $this->default;
+    }
+}
