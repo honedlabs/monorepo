a2c84942f287f1f98db42e7a9211c820837e1527
jdw5 <joshuadwallace1@gmail.com>
2025-10-08 08:40:14 +1000

    fix(lang): using prepend instead of priority list prepend

--- packages/laravel/lang/workbench/bootstrap/app.php
+++ packages/laravel/lang/workbench/bootstrap/app.php
@@ -14,10 +14,11 @@ return Application::configure(basePath: $APP_BASE_PATH ?? default_skeleton_path(
         commands: __DIR__.'/../routes/console.php',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->prependToPriorityList(
-            before: Illuminate\Routing\Middleware\SubstituteBindings::class,
-            prepend: Honed\Lang\Middleware\Localize::class,
-        );
+        $middleware->prepend(Honed\Lang\Middleware\Localize::class);
+        // $middleware->prependToPriorityList(
+        //     before: Illuminate\Routing\Middleware\SubstituteBindings::class,
+        //     prepend: Honed\Lang\Middleware\Localize::class,
+        // );
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
