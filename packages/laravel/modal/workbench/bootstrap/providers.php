5e3d092dd7315f43f9e08ece96714258493057e3
jdw5 <joshuadwallace1@gmail.com>
2025-10-25 16:15:30 +1000

    feat(modal): changed tests to stable only

--- packages/laravel/modal/workbench/bootstrap/providers.php
+++ packages/laravel/modal/workbench/bootstrap/providers.php
@@ -6,5 +6,4 @@ return [
     App\Providers\WorkbenchServiceProvider::class,
     Honed\Modal\ModalServiceProvider::class,
     Inertia\ServiceProvider::class,
-    Illuminate\Foundation\Support\Providers\RouteServiceProvider::class,
 ];
