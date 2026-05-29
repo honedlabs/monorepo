36727944be5ca55566d568e4363e710d0b4bc2c6
jdw5 <joshuadwallace1@gmail.com>
2026-05-29 19:26:09 +1000

    feat(stats): release

--- packages/vue/stats/dist/index.es.js
+++ packages/vue/stats/dist/index.es.js
@@ -1,20 +1,18 @@
-import { computed as a } from "vue";
-function c(e) {
-  const s = a(
-    () => e._values.map((t) => ({
-      ...t,
-      stat: () => n(t.name)
-    }))
-  ), u = a(() => e._stat_key);
-  function n(t) {
-    return e[t];
-  }
-  return {
-    values: s,
-    statKey: u,
-    getStat: n
-  };
+import { computed as e } from "vue";
+//#region src/stats.ts
+function t(t) {
+	let n = e(() => t._values.map((e) => ({
+		...e,
+		stat: () => i(e.name)
+	}))), r = e(() => t._stat_key);
+	function i(e) {
+		return t[e];
+	}
+	return {
+		values: n,
+		statKey: r,
+		getStat: i
+	};
 }
-export {
-  c as useStats
-};
+//#endregion
+export { t as useStats };
